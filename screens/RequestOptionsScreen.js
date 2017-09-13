0c591659bd6221dc1914587a1d709e47b02724a1
lucaslunasouza <lucas.lunasouza@mail.utoronto.ca>
2017-09-13 04:48:01 -0400

    Added Body Tab to Request Options Screen

--- screens/RequestOptionsScreen.js
+++ screens/RequestOptionsScreen.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {
   Platform,
   ScrollView,
-  TouchableOpacity,
   View,
   Text,
   StatusBar,
@@ -15,6 +14,9 @@ import {
   List,
   ListItem,
   Icon,
+  Tab,
+  Tabs,
+  Fab,
 } from 'native-base';
 
 import { Col, Row, Grid } from 'react-native-easy-grid';
@@ -38,14 +40,15 @@ export default class ParametersScreen extends React.Component {
     super(props);
     this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1._id !== r2._id });
     this.state = {
-      showParameters: true,
+      fabActive: 'true',
+      bodyType: 'form-data',
     };
   }
 
-  deleteRow(secId, rowId, rowMap) {
+  deleteRow(tab, secId, rowId, rowMap) {
     // Delete row where slide button was pressed
     rowMap[`${secId}${rowId}`].props.closeRow();
-    if (this.state.showParameters) {
+    if (tab === 'Parameters') {
       const newData = [...this.props.screenProps.requestParameters];
       newData.splice(rowId, 1);
       this.props.screenProps.requestParameters = newData;
@@ -57,9 +60,9 @@ export default class ParametersScreen extends React.Component {
     this.forceUpdate();
   }
 
-  addRow() {
+  addRow(tab) {
     // Add a row to request parameters or headers, depending on which is currently being shown
-    if (this.state.showParameters) {
+    if (tab === 'Parameters') {
       const newData = [...this.props.screenProps.requestParameters];
       newData.push(['', '']);
       this.props.screenProps.requestParameters = newData;
@@ -77,73 +80,166 @@ export default class ParametersScreen extends React.Component {
         <View style={[this.props.screenProps.theme.requestContainer,
           { position: 'absolute', flexDirection: 'column', height: '100%', width: '100%' }]}
         >
-          <Row style={{ flex: 0.08 }}>
-            <Col size={50} style={{ justifyContent: 'center', borderColor: '#bcccd1', borderWidth: 0.5 }}>
-              <TouchableOpacity onPress={() => this._handleSwitchParametersView('parameters')}>
-                <Text style={this._handleParametersStyle('parameters')}>Parameters</Text>
-              </TouchableOpacity>
-            </Col>
-            <Col size={50} style={{ justifyContent: 'center', borderColor: '#bcccd1', borderWidth: 0.5 }}>
-              <TouchableOpacity onPress={() => this._handleSwitchParametersView('headers')}>
-                <Text style={this._handleParametersStyle('headers')}>Headers</Text>
-              </TouchableOpacity>
-            </Col>
-          </Row>
-          <ScrollView style={{ flex: 0.8 }}>
-            <List
-              dataSource={this.ds.cloneWithRows(this._handleParameters())}
-              enableEmptySections
-              renderRow={(data, secId, rowId, rowMap) =>
-                (<ListItem style={{ height: 50, width: '100%' }}>
-                  <Grid>
-                    <Row>
-                      <Col size={50} >
-                        <Input
-                          placeholder='Key'
-                          type='text'
-                          style={{ textAlign: 'center' }}
-                          value={data[0]}
-                          onChangeText={(text) => this._handleUpdateList(text, secId, rowId, rowMap, 0)}
-                        />
-                      </Col>
-                      <Col size={50}>
-                        <Input
-                          type='text'
-                          placeholder='Value'
-                          style={{ textAlign: 'center' }}
-                          value={data[1]}
-                          onChangeText={(text) => this._handleUpdateList(text, secId, rowId, rowMap, 1)}
-                        />
-                      </Col>
-                    </Row>
-                  </Grid>
-                </ListItem>)}
-              renderLeftHiddenRow={() =>
-                ((<Button full>
-                  <Icon active name="information-circle" />
-                </Button>))}
-              renderRightHiddenRow={(data, secId, rowId, rowMap) =>
-                (<Button full danger style={{ height: '100%' }} onPress={() => this.deleteRow(secId, rowId, rowMap)}>
-                  <Icon active name="trash" />
-                </Button>)}
-              leftOpenValue={0}
-              rightOpenValue={-75}
-            />
-            <Button
-              onPress={() => this.addRow()}
-              style={{ backgroundColor: 'white', height: 50, width: '100%', justifyContent: 'center', borderColor: '#bcccd1', borderWidth: 0.5 }}
+          <Tabs
+            initialPage={0}
+            tabBarUnderlineStyle={{ backgroundColor: Colors.mainTheme }}
+          >
+            <Tab
+              heading="Parameters"
+              tabStyle={{ ...this.props.screenProps.theme.tabBar }}
+              textStyle={{ ...this.props.screenProps.theme.text }}
+              activeTabStyle={{ ...this.props.screenProps.theme.tabBar }}
+              activeTextStyle={{ color: Colors.mainTheme }}
             >
-              <Text>New</Text>
-            </Button>
-          </ScrollView>
+              <ScrollView style={{ flex: 0.8 }}>
+                <List
+                  dataSource={this.ds.cloneWithRows(this.props.screenProps.requestParameters)}
+                  enableEmptySections
+                  renderRow={(data, secId, rowId, rowMap) =>
+                    (<ListItem style={{ height: 50, width: '100%' }}>
+                      <Grid>
+                        <Row>
+                          <Col size={50} >
+                            <Input
+                              placeholder='Key'
+                              type='text'
+                              style={{ textAlign: 'center' }}
+                              value={data[0]}
+                              onChangeText={(text) => this._handleUpdateList('Parameters', text, secId, rowId, rowMap, 0)}
+                            />
+                          </Col>
+                          <Col size={50}>
+                            <Input
+                              type='text'
+                              placeholder='Value'
+                              style={{ textAlign: 'center' }}
+                              value={data[1]}
+                              onChangeText={(text) => this._handleUpdateList('Parameters', text, secId, rowId, rowMap, 1)}
+                            />
+                          </Col>
+                        </Row>
+                      </Grid>
+                    </ListItem>)}
+                  renderLeftHiddenRow={() =>
+                    ((<Button full>
+                      <Icon active name="information-circle" />
+                    </Button>))}
+                  renderRightHiddenRow={(data, secId, rowId, rowMap) =>
+                    (<Button full danger style={{ height: '100%' }} onPress={() => this.deleteRow('Parameters', secId, rowId, rowMap)}>
+                      <Icon active name="trash" />
+                    </Button>)}
+                  leftOpenValue={0}
+                  rightOpenValue={-75}
+                />
+                <Button
+                  onPress={() => this.addRow('Parameters')}
+                  style={{ backgroundColor: 'white', height: 50, width: '100%', justifyContent: 'center', borderColor: '#bcccd1', borderWidth: 0.5 }}
+                >
+                  <Text>New</Text>
+                </Button>
+              </ScrollView>
+            </Tab>
+            <Tab
+              heading="Headers"
+              tabStyle={{ ...this.props.screenProps.theme.tabBar }}
+              textStyle={{ ...this.props.screenProps.theme.text }}
+              activeTabStyle={{ ...this.props.screenProps.theme.tabBar }}
+              activeTextStyle={{ color: Colors.mainTheme }}
+            >
+              <ScrollView style={{ flex: 0.8 }}>
+                <List
+                  dataSource={this.ds.cloneWithRows(this.props.screenProps.requestHeaders)}
+                  enableEmptySections
+                  renderRow={(data, secId, rowId, rowMap) =>
+                    (<ListItem style={{ height: 50, width: '100%' }}>
+                      <Grid>
+                        <Row>
+                          <Col size={50} >
+                            <Input
+                              placeholder='Key'
+                              type='text'
+                              style={{ textAlign: 'center' }}
+                              value={data[0]}
+                              onChangeText={(text) => this._handleUpdateList('Headers', text, secId, rowId, rowMap, 0)}
+                            />
+                          </Col>
+                          <Col size={50}>
+                            <Input
+                              type='text'
+                              placeholder='Value'
+                              style={{ textAlign: 'center' }}
+                              value={data[1]}
+                              onChangeText={(text) => this._handleUpdateList('Headers', text, secId, rowId, rowMap, 1)}
+                            />
+                          </Col>
+                        </Row>
+                      </Grid>
+                    </ListItem>)}
+                  renderLeftHiddenRow={() =>
+                    ((<Button full>
+                      <Icon active name="information-circle" />
+                    </Button>))}
+                  renderRightHiddenRow={(data, secId, rowId, rowMap) =>
+                    (<Button full danger style={{ height: '100%' }} onPress={() => this.deleteRow('Headers', secId, rowId, rowMap)}>
+                      <Icon active name="trash" />
+                    </Button>)}
+                  leftOpenValue={0}
+                  rightOpenValue={-75}
+                />
+                <Button
+                  onPress={() => this.addRow()}
+                  style={{ backgroundColor: 'white', height: 50, width: '100%', justifyContent: 'center', borderColor: '#bcccd1', borderWidth: 0.5 }}
+                >
+                  <Text>New</Text>
+                </Button>
+              </ScrollView>
+            </Tab>
+            <Tab
+              heading="Body"
+              tabStyle={{ ...this.props.screenProps.theme.tabBar }}
+              textStyle={{ ...this.props.screenProps.theme.text }}
+              activeTabStyle={{ ...this.props.screenProps.theme.tabBar }}
+              activeTextStyle={{ color: Colors.mainTheme }}
+            >
+              <Fab
+                active={this.state.active}
+                direction="up"
+                containerStyle={{ marginLeft: 10 }}
+                style={{ backgroundColor: Colors.mainTheme }}
+                position="bottomRight"
+                onPress={() => this.setState({ active: !this.state.active })}
+              >
+                <Icon name="list" />
+                <Button
+                  style={{ backgroundColor: '#34A34F' }}
+                  onPress={() => this.setState({ bodyType: 'form-data' })}
+                >
+                  <Text style={{ textAlign: 'center', color: 'white' }}>form-data</Text>
+                </Button>
+                <Button
+                  style={{ backgroundColor: '#3B5998' }}
+                  onPress={() => this.setState({ bodyType: 'x-www-form-urlencoded' })}
+                >
+                  <Text style={{ textAlign: 'center', color: 'white' }}>x-www-form-urlencoded</Text>
+                </Button>
+                <Button
+                  style={{ backgroundColor: '#DD5144' }}
+                  onPress={() => this.setState({ bodyType: 'raw' })}
+                >
+                  <Text style={{ textAlign: 'center', color: 'white' }}>raw</Text>
+                </Button>
+              </Fab>
+              { this._handleBody() }
+            </Tab>
+          </Tabs>
         </View>
       </View>
     );
   }
-  _handleUpdateList(text, secId, rowId, rowMap, index) {
+  _handleUpdateList(tab, text, secId, rowId, rowMap, index) {
     // Updates list based on user input
     rowMap[`${secId}${rowId}`].props.closeRow();
-    if (this.state.showParameters) {
+    if (tab === 'Parameters') {
       const newData = [...this.props.screenProps.requestParameters];
       newData[rowId][index] = text;
       this.props.screenProps.requestParameters = newData;
@@ -154,40 +250,13 @@ export default class ParametersScreen extends React.Component {
     }
     this.forceUpdate();
   }
-
-  _handleParameters() {
-    if (this.state.showParameters) {
-      return this.props.screenProps.requestParameters;
+  _handleBody() {
+    if (this.state.bodyType === 'form-data') {
+      return (<Text>form-data</Text>);
+    } else if (this.state.bodyType === 'x-www-form-urlencoded') {
+      return (<Text>x-www-form-urlencoded</Text>);
     }
-    return this.props.screenProps.requestHeaders;
-  }
-
-  _handleParametersStyle = (name) => {
-    // Changes the colour of the currently selected tab in the parameters modal
-    if (this.state.showParameters && name === 'parameters') {
-      return [styles.viewTab, styles.viewTabSelected];
-    } else if (!this.state.showParameters && name === 'headers') {
-      return [styles.viewTab, styles.viewTabSelected];
-    }
-    return [styles.viewTab, this.props.screenProps.theme.text];
-  }
-
-  _handleSwitchResponseView = (name) => {
-    // Switches view when tab is changed in main/response modal
-    if (name === 'body') {
-      this.setState({ showResponseBody: true });
-    } else {
-      this.setState({ showResponseBody: false });
-    }
-  }
-
-  _handleSwitchParametersView = (name) => {
-    // Switches view when tab is changed in parameters modal
-    if (name === 'parameters') {
-      this.setState({ showParameters: true });
-    } else {
-      this.setState({ showParameters: false });
-    }
+    return (<Text>raw</Text>);
   }
 }
 
